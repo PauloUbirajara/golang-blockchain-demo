0bb03bc8273a6c7f152e94b1b2824180fe2f3d63
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-25 23:46:34 -0600

    create Blockchain struct to store Blocks in array

---
+++ basic/blockchain.go
@@ -0,0 +1,36 @@
+package basic
+
+type Blockchain struct {
+	Blocks []Block
+}
+
+func (bc *Blockchain) NewBlock(content string, difficulty int) {
+	DEFAULT_PREVIOUS_HASH := "secret"
+
+	var lastBlock Block
+	lastBlock.CurrentHash = DEFAULT_PREVIOUS_HASH
+
+	if len(bc.Blocks) > 0 {
+		lastBlock = bc.Blocks[len(bc.Blocks)-1]
+	}
+
+	newBlock := Block{
+		Index:        lastBlock.Index + 1,
+		Content:      content,
+		PreviousHash: lastBlock.CurrentHash,
+	}
+
+	newBlock.SearchHash(difficulty)
+
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func (bc *Blockchain) StartBlockchain(previousBlockArray []Block) {
+	bc.Blocks = previousBlockArray
+}
+
+func (bc *Blockchain) PrintBlocks() {
+	for _, b := range bc.Blocks {
+		b.Print()
+	}
+}
