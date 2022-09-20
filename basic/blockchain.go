74b730c61f67678589a19f2445c799d0552b3c6a
PauloUbirajara <ubirajara3@hotmail.com>
2022-09-20 00:20:37 -0300

    add regexp validation for user wallet creation
    add regexp validation for transaction between users
    
    simplify validation into smaller functions
    
    add usage examples in main.go

--- basic/blockchain.go
+++ basic/blockchain.go
@@ -47,27 +47,142 @@ func (bc *Blockchain) PrintBlocks() {
 	}
 }
 
-func (bc *Blockchain) Validate() bool {
-	for i := 0; i < len(bc.Blocks)-1; i++ {
-		currentBlock := bc.Blocks[i]
+func (bc *Blockchain) ValidateBlockSequence(currentBlock Block, nextBlock Block) bool {
+	expectedHash := currentBlock.CurrentHash
+	currentBlock.SearchHash(currentBlock.Difficulty)
 
-		expectedHash := currentBlock.CurrentHash
-		currentBlock.SearchHash(currentBlock.Difficulty)
-
-		for i := range currentBlock.CurrentHash {
-			if currentBlock.CurrentHash[i] != expectedHash[i] {
-				return false
-			}
-		}
-
-		nextBlock := bc.Blocks[i+1]
-
-		if currentBlock.CurrentHash != nextBlock.PreviousHash {
+	for i := range currentBlock.CurrentHash {
+		if currentBlock.CurrentHash[i] != expectedHash[i] {
 			return false
 		}
 	}
 
 	return true
+}
+
+func (bc *Blockchain) ValidateSingleTransaction(userWallets map[string]float64, transaction string) bool {
+	transactionBetweenUsers := strings.Split(transaction, " > ")
+
+	// Verificar se primeiro usuário previamente criado com carteira
+	firstUser := transactionBetweenUsers[0]
+	firstUserValue, firstUserExists := userWallets[firstUser]
+
+	if !firstUserExists {
+		return false
+	}
+
+	// Verificar se segundo usuário previamente criado com carteira
+	secondUser := transactionBetweenUsers[2]
+	_, secondUserExists := userWallets[secondUser]
+
+	if !secondUserExists {
+		return false
+	}
+
+	valueToTransfer, err := strconv.ParseFloat(transactionBetweenUsers[1], 64)
+
+	// Verificar por erro ao converter valor em string para float64
+	if err != nil {
+		return false
+	}
+
+	// Verificar se transação transformará o primeiro usuário em saldo negativo
+	if firstUserValue-valueToTransfer < 0 {
+		return false
+	}
+
+	// Alterar, na referência da carteira dos usuários, o novo valor dos usuários da transação
+	userWallets[firstUser] -= valueToTransfer
+	userWallets[secondUser] += valueToTransfer
+
+	return true
+}
+
+func (bc *Blockchain) ValidateUserTransactions(userWallets map[string]float64) bool {
+	USER_TRANSACTION_REGEX := `^[a-zA-Z]+ > [0-9]+(\.[0-9]+)? > [a-zA-Z]+$`
+
+	// Verificar se transação respeita padrão A > VALOR > B
+	re, err := regexp.Compile(USER_TRANSACTION_REGEX)
+
+	if err != nil {
+		return false
+	}
+
+	for _, block := range bc.Blocks[1:] {
+		for _, trx := range block.Content {
+			matched := re.Match([]byte(trx))
+
+			if !matched {
+				return false
+			}
+
+			if !bc.ValidateSingleTransaction(userWallets, trx) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (bc *Blockchain) ValidateUserWalletCreation() bool {
+	// Definindo carteiras
+	userWallets := make(map[string]float64)
+
+	USER_WALLET_REGEX := `^[a-zA-Z]+ = [0-9]+$`
+
+	re, err := regexp.Compile(USER_WALLET_REGEX)
+
+	if err != nil {
+		return false
+	}
+
+	for _, trx := range bc.Blocks[0].Content {
+		// Verificar se criação de carteira segue padrão A = VALOR
+		matched := re.Match([]byte(trx))
+
+		if err != nil || !matched {
+			return false
+		}
+
+		currentUserWallet := strings.Split(trx, " = ")
+
+		user := currentUserWallet[0]
+
+		// Verifica se carteira previamente criada
+		_, userAlreadyCreated := userWallets[user]
+		if userAlreadyCreated {
+			return false
+		}
+
+		value, err := strconv.ParseFloat(currentUserWallet[1], 64)
+
+		if err != nil {
+			return false
+		}
+
+		// Cria nova carteira e atribui valor
+		userWallets[user] = value
+	}
+
+	return bc.ValidateUserTransactions(userWallets)
+}
+
+func (bc *Blockchain) Validate() bool {
+	for i := 0; i < len(bc.Blocks)-1; i++ {
+		currentBlock := bc.Blocks[i]
+		nextBlock := bc.Blocks[i+1]
+
+		if currentBlock.CurrentHash != nextBlock.PreviousHash {
+			return false
+		}
+
+		if !bc.ValidateBlockSequence(currentBlock, nextBlock) {
+			return false
+		}
+	}
+
+	return bc.ValidateUserWalletCreation()
 }
 
 func (bc *Blockchain) SaveToJSON(outputName string) {
--- main.go
+++ main.go
@@ -5,18 +5,49 @@ import (
 	b "golang-blockchain-demo/basic"
 )
 
+func GoodExample1(blockchain *b.Blockchain) {
+	// Carteiras criadas corretamente e transações respeitam limites de conta
+	DIFFICULTY := 3
+	blockchain.NewBlock(DIFFICULTY, "A = 10", "B = 20")
+	blockchain.NewBlock(DIFFICULTY, "A > 10 > B")
+	blockchain.NewBlock(DIFFICULTY, "B > 2 > A", "A > 1 > B", "B > 9 > A")
+}
+
+func BadExample1(blockchain *b.Blockchain) {
+	// Carteira não criada conforme padrão
+	DIFFICULTY := 3
+	blockchain.NewBlock(DIFFICULTY, "A != 10", "B = 20")
+}
+
+func BadExample2(blockchain *b.Blockchain) {
+	// Carteira criada em duplicidade
+	DIFFICULTY := 3
+	blockchain.NewBlock(DIFFICULTY, "A = 10", "A = 20")
+}
+
+func BadExample3(blockchain *b.Blockchain) {
+	// Transação para uma carteira inexistente
+	DIFFICULTY := 3
+	blockchain.NewBlock(DIFFICULTY, "A = 10", "B = 10")
+	blockchain.NewBlock(DIFFICULTY, "A > 5 > C")
+}
+
+func BadExample4(blockchain *b.Blockchain) {
+	// Transação causa valor negativo em carteira
+	DIFFICULTY := 3
+	blockchain.NewBlock(DIFFICULTY, "A = 10", "B = 10")
+	blockchain.NewBlock(DIFFICULTY, "A > 20 > B")
+}
+
 func main() {
 	var blockchain b.Blockchain
-	// blockchain.LoadFromJSON("blocks.json")
 
-	// Adicionar N blocos, incluindo o genesis se "blockchain" iniciar vazio
-	DIFFICULTY := 3
-	BLOCK_COUNT := 3
-	for i := 0; i < BLOCK_COUNT; i++ {
-		blockchain.NewBlock(DIFFICULTY, "alo", "hello", "ola", "oi2")
-	}
+	// GoodExample1(&blockchain)
+	// BadExample1(&blockchain)
+	// BadExample2(&blockchain)
+	// BadExample3(&blockchain)
+	BadExample4(&blockchain)
 
 	blockchain.PrintBlocks()
 	fmt.Println("Validado", blockchain.Validate())
-	// blockchain.SaveToJSON("blocks.json")
 }
